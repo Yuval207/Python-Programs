306835bfbe2cb960e120588ce1f6ff2be551bbdc
Yuval <yuvalsharma49@gmail.com>
2023-04-25 04:28:56 +0530

    Adding ColorDetection.py

--- OpenCV/Drawing.py
+++ OpenCV/Drawing.py
@@ -9,10 +9,12 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
-    img = cv2.line(frame, (0, height), (width, 0), (0, 255, 0), 5)
-    img = cv2.rectangle(frame, (100, 100), (200, 200), (128, 128, 128), 5)
-    img = cv2.circle(frame, (300, 300), 60, (0, 0, 255), -1)
+    # img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 10)
+    # img = cv2.line(frame, (0, height), (width, 0), (0, 255, 0), 5)
+    img = cv2.rectangle(frame, (60, 50), (150, 250), (29.4, 29.4, 58.8), 5)
+    img = cv2.circle(frame, (60, 280), 60, (19, 139, 69), -1)
+    img = cv2.circle(frame, (150, 280), 60, (19, 139, 69), -1)
+    
 
     font = cv2.FONT_HERSHEY_TRIPLEX
     img = cv2.putText(img, 'I am nice',(100, height-20), font, 2, (69, 69, 69), 5, cv2.LINE_AA)
